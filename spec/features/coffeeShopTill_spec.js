886f5191bed1a03cd7d3ffab7442af29bd91690a
Sara <srancati0@gmail.com>
2020-01-31 19:26:16 +0000

    Add feature test

--- spec/features/coffeeShopTill_spec.js
+++ spec/features/coffeeShopTill_spec.js
@@ -1,6 +1,7 @@
 describe ("Coffee Shop Till", function(){
     
     describe("#showBill", function(){
+
         it("should format the bill when order is 1 Cappuccino", function(){
             let order = new Order()
             order.addItem("Cappuccino", 1)
@@ -14,6 +15,15 @@ describe ("Coffee Shop Till", function(){
             order.addItem("Tea", 3)
             let display = new Display(order)
             expect(display.issueBill()).toEqual("The Coffee Connection<br><br>123 Lakeside Way<br>Phone: +16503600708<br>.<br>Cappuccino 1 x $3.85<br>Tea 3 x $3.65<br><br>Tax: $1.28<br>Total: $16.08<br>Thank you!")
+        })
+
+        it("should format the bill when order is 3 Cappuccinos, 1 Tea, 2 Flat Whites", function(){
+            let order = new Order()
+            order.addItem("Cappuccino", 1)
+            order.addItem("Tea", 1)
+            order.addItem("Flat White", 2)
+            let display = new Display(order)
+            expect(display.issueBill()).toEqual("The Coffee Connection<br><br>123 Lakeside Way<br>Phone: +16503600708<br>.<br>Cappuccino 1 x $3.85<br>Tea 1 x $3.65<br>Flat White 2 x $4.75<br><br>Tax: $1.47<br>Total: $18.47<br>Thank you!")
         })
     }) 
 
