a20923f959978fb054adac3a2e9383a9e27fee2a
Sara <srancati0@gmail.com>
2020-02-02 14:56:44 +0000

    Add button and logic for selecting from dropdown menu

--- src/interface.js
+++ src/interface.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 
     let orderOne = new Order;
-    orderOne.addItem("Tea", 1)
-    // orderOne.addItem("Cafe Latte", 4)
-    // orderOne.addItem("Cappuccino", 4)
     let displayOne = new Display(orderOne);
-
-
+    $('#button1').click(function( event ) {
+        event.preventDefault();
+        var item = $("#menu_item").val();
+        orderOne.addItem(item, 1)
+        $('#order_list').html(displayOne.formatOrder())
+    });
+    
     $('#button2').click(function( event ) {
         event.preventDefault();
         $('#display_bill').html(displayOne.issueBill())
