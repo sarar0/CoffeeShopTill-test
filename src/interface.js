65032f2381974fa459e7346930b5996a15f7e5c1
Sara <srancati0@gmail.com>
2020-02-02 14:33:08 +0000

    Add button and logic to calculate total

--- src/interface.js
+++ src/interface.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-    $( function() {
-      $( "#menu_item" ).selectmenu();
-    });
 
     let orderOne = new Order;
-    // orderOne.addItem("Tea", 1)
+    orderOne.addItem("Tea", 1)
     // orderOne.addItem("Cafe Latte", 4)
     // orderOne.addItem("Cappuccino", 4)
     let displayOne = new Display(orderOne);
 
-    $('#display_bill').html(displayOne.issueBill())
 
+    $('#button2').click(function( event ) {
+        event.preventDefault();
+        $('#display_bill').html(displayOne.issueBill())
+    })
 })
